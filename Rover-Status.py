db3d5aed456af5489939c6e50936818a6e0c0338
ressut <47404112+ressut@users.noreply.github.com>
2024-10-10 22:30:32 +0100

    Update Rover-Status.py

--- Rover-Status.py
+++ Rover-Status.py
@@ -10,7 +10,7 @@ battery_usage = 0.027
 time_to_die = battery_total/battery_usage
 time = float(input("Enter the time of the travel:"))
 if (time >= time_to_die):
-    print("The Rover Battery Has Died")
+    print("The Rover Battery will need to be Re-charded after reaching the final destination")
     exit()
 #Converting Angle
 angle_radians = math.radians(angle_degrees)
@@ -20,12 +20,15 @@ speed = 1.5
 distance = speed*time
 battery_consumed = battery_usage*time
 battery_remaining = battery_total-battery_consumed
-horizontal = distance*math.cos(angle_radians)
-vertical = distance*math.sin(angle_radians)
+horizontal = distance*math.sin(angle_radians)
+vertical = distance*math.cos(angle_radians)
 #Output
 print(f"Angle: {angle_degrees} Time: {time}")
 print(f"Distance Travelled: {distance}")
 print(f"Horizontal Movement: {horizontal:.2f}")
 print(f"Vertical Movement: {vertical:.2f}")
 print(f"Battery Usage: {battery_consumed:.2f}")
-print(f"Battery Remaining: {battery_remaining}")
+print(f"Battery Remaining: {battery_remaining:.2f}")
+if (battery_remaining <= 10):
+    print(f"Battery Remaining is low")
+
