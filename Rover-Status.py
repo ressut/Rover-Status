af06d8c80dc18506aee540b5908f29e7c38bb601
ressut <47404112+ressut@users.noreply.github.com>
2024-10-10 22:17:47 +0100

    Update Rover-Status.py

--- Rover-Status.py
+++ Rover-Status.py
@@ -4,21 +4,24 @@ import math
 angle_degrees = float(input("Enter the Angle between 0 and 90:"))
 if (angle_degrees > 90) or (angle_degrees < 0):
     print("The angle value must be between 0 and 90:")
+    exit()
+battery_total = 100
+battery_usage = 0.027
+time_to_die = battery_total/battery_usage
 time = float(input("Enter the time of the travel:"))
+if (time >= time_to_die):
+    print("The Rover Battery Has Died")
+    exit()
 #Converting Angle
 angle_radians = math.radians(angle_degrees)
 #Constants
-battery_total = 100
-battery_usage = 0.027
 speed = 1.5
-travel_parameters = 0-90
 #Calculation
 distance = speed*time
 battery_consumed = battery_usage*time
 battery_remaining = battery_total-battery_consumed
 horizontal = distance*math.cos(angle_radians)
 vertical = distance*math.sin(angle_radians)
-
 #Output
 print(f"Angle: {angle_degrees} Time: {time}")
 print(f"Distance Travelled: {distance}")
