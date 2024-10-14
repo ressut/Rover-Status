b7a4f184fcb993b7e220e08ba3eb3a1577510964
ressut <47404112+ressut@users.noreply.github.com>
2024-10-14 19:01:13 +0100

    Update Rover-Status.py

--- Rover-Status.py
+++ Rover-Status.py
@@ -1,5 +1,5 @@
 import math
-#Start Progam
+
 #Input
 angle_degrees = float(input("Enter the Angle between 0° and 90°:"))
 if (angle_degrees > 90) or (angle_degrees < 0):
@@ -9,7 +9,7 @@ battery_total = 100
 battery_usage = 0.027 # battery comsume per second
 time_to_die = battery_total/battery_usage
 time = float(input("Enter the time of the travel in seconds:"))
-if (time >= time_to_die):
+if (time > time_to_die):
     print("The Rover Battery will need to be Re-charded after reaching the final destination")
     exit()
 #Converting Angle
@@ -19,7 +19,7 @@ speed = 1.5 # meters/second
 #Calculation
 distance = speed*time
 battery_consumed = battery_usage*time
-battery_remaining = round(battery_total-battery_consumed, 2)
+battery_remaining = round(battery_total-battery_consumed, 2) #value needed to be rounded in order to elif to work
 horizontal = distance*math.sin(angle_radians)
 vertical = distance*math.cos(angle_radians)
 #Output
@@ -33,3 +33,4 @@ if battery_remaining > 0 and battery_remaining <= 10:
     print(f"Battery Remaining is low!")
 elif battery_remaining <= 0:
     print(f"The Rover battery needs Re-charge!")
+    exit()
