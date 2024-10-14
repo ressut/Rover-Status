6826d78d20ad1d1d6dae53f8d7183a6c6943b5d2
ressut <47404112+ressut@users.noreply.github.com>
2024-10-14 10:56:34 +0100

    Update Rover-Status.py

--- Rover-Status.py
+++ Rover-Status.py
@@ -3,13 +3,13 @@ import math
 #Input
 angle_degrees = float(input("Enter the Angle between 0° and 90°:"))
 if (angle_degrees > 90) or (angle_degrees < 0):
-    print("Error: Angle value must be between 0° and 90°:")
+    print("Error: Angle value must be between 0° and 90°.")
     exit()
 battery_total = 100
 battery_usage = 0.027 # battery comsume per second
 time_to_die = battery_total/battery_usage
 time = float(input("Enter the time of the travel in seconds:"))
-if (time >= time_to_die):
+if (time > time_to_die):
     print("The Rover Battery will need to be Re-charded after reaching the final destination")
     exit()
 #Converting Angle
@@ -19,15 +19,17 @@ speed = 1.5 # meters/second
 #Calculation
 distance = speed*time
 battery_consumed = battery_usage*time
-battery_remaining = battery_total-battery_consumed
+battery_remaining = round(battery_total-battery_consumed, 2)
 horizontal = distance*math.sin(angle_radians)
 vertical = distance*math.cos(angle_radians)
 #Output
-print(f"Angle: {angle_degrees}° Time: {time} seconds")
-print(f"Distance Travelled: {distance} meters")
+print(f"Angle: {angle_degrees}° Time: {time:.2f} seconds")
+print(f"Distance Travelled: {distance:.2f} meters")
 print(f"Horizontal Movement: {horizontal:.2f} meters")
 print(f"Vertical Movement: {vertical:.2f} meters")
 print(f"Battery Usage: {battery_consumed:.2f}%")
 print(f"Battery Remaining: {battery_remaining:.2f}%")
-if (battery_remaining <= 10):
+if (battery_remaining < 0) and (battery_remaining <= 10):
     print(f"Battery Remaining is low!")
+elif (battery_remaining <= 0):
+    print(f"The Rover battery needs Re-charge!")
